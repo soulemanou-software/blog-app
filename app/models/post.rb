6ce1bdef9e5a85f816a8cc1303e588e80e41de07
soulemanou kunchiefuh babanou <kunchiefuh@gmail.com>
2023-01-21 19:44:51 +0100

    blog app 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  has_many :comments, foreign_key: :post_id
+
+  has_many :likes, foreign_key: :post_id
+
+  def update_post_count
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).includes(:post).limit(5)
+  end
 end
